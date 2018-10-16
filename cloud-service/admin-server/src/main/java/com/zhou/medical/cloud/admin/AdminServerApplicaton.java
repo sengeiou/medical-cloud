b08ad81cd3061ee676e67be8b582b825ad7f1409
zhouzhouluo <zhouzhouluo@gmail.com>
2018-10-16 14:48:24 +0800

    cloud-service

---
+++ cloud-service/admin-server/src/main/java/com/zhou/medical/cloud/admin/AdminServerApplicaton.java
@@ -0,0 +1,23 @@
+package com.zhou.medical.cloud.admin;
+
+import de.codecentric.boot.admin.config.EnableAdminServer;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.SecurityAutoConfiguration;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.Configuration;
+
+
+@SpringBootApplication(exclude = {SecurityAutoConfiguration.class })
+@EnableAdminServer
+@Configuration
+@EnableAutoConfiguration
+@EnableDiscoveryClient
+public class AdminServerApplicaton {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AdminServerApplicaton.class, args);
+	}
+
+}
