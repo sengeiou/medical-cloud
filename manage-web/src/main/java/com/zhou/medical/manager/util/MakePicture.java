3391cb53aeb53d02679b83ac178d7a4bb81b6b4f
zhouzhouluo <zhouzhouluo@gmail.com>
2018-11-15 14:50:39 +0800

    运营服务管理平台，管理服务

--- manage-web/src/main/java/com/zhou/medical/manager/util/MakePicture.java
+++ manage-web/src/main/java/com/zhou/medical/manager/util/MakePicture.java
@@ -24,22 +24,27 @@ public class MakePicture  //产生识别验证图像
 			
 		BufferedImage image=new BufferedImage(width, height,BufferedImage.TYPE_INT_RGB) ;
 		Graphics g=image.getGraphics()  ;
-		g.setColor(Color.LIGHT_GRAY) ;
+//		g.setColor(Color.LIGHT_GRAY) ;
 		g.fillRect(0, 0, width, height)  ;
-		g.setColor(new Color(0x5265fd)) ;
-		g.drawRect(0, 0, width, height)  ;  
+//		g.setColor(new Color(0x5265fd)) ;
+//		g.drawRect(0, 0, width-1, height-1)  ;
 		String str ="" ;
 		for(int x=0;x<4;x++)
 		{
 		  str+=charTable[(int) (Math.random()*charTable.length)];
 		}
-		g.setFont(new Font("Tahoma", Font.BOLD, 12));
+		g.setFont(new Font("Times New Roman", Font.PLAIN, 14));
+		g.setColor(new Color(0x9E2A30)) ;
 		g.drawString(str.substring(0, 1), 5, 15);
+		g.setColor(new Color(0xE40E18)) ;
 		g.drawString(str.substring(1, 2), 20, 17);
-		g.drawString(str.substring(2, 3), 35, 19);
+		g.setColor(new Color(0x071187)) ;
+		g.drawString(str.substring(2, 3), 35, 13);
+		g.setColor(new Color(0x068513)) ;
 		g.drawString(str.substring(3, 4), 50, 16);
 		Random rand=new Random() ;
-		for(int i=0;i<10;i++)
+		g.setColor(Color.LIGHT_GRAY) ;
+		for(int i=0;i<20;i++)
 		{
 		  int x=rand.nextInt(width)  ;
 		  int y=rand.nextInt(height)  ;	
